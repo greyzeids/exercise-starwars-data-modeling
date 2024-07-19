2cc82ee3b5291dc82ee197fc465e1aee6d77a8c0
Miquel Carnot <64230328+greyzeids@users.noreply.github.com>
2024-07-19 12:02:41 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,23 +7,49 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    favourites = relationship('Favourite', backref='user', lazy=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name_id = Column(String(100), nullable=False)
+    gender = Column(String(10))
+    hair_color = Column(String(30))
+    eye_color = Column(String(30))
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name_id = Column(String(100), nullable=False)
+    climate = Column(String(50))
+    population = Column(Integer)
+    terrain = Column(String(50))
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name_id = Column(String(100), nullable=False)
+    model = Column(String(50))
+    manufacturer = Column(String(50))
+    cost_in_credits = Column(Integer)
+
+class Favourite(Base):
+    __tablename__ = 'favourite'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    char_id = Column(Integer, ForeignKey('character.id'), nullable=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'), nullable=True)
+    add = Column(String(50))
+    delete = Column(String(50))
 
     def to_dict(self):
         return {}
